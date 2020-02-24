7f8e60ef27bf0464cb2a748c026f9a492246ffb1
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-02-24 17:49:15 +0000

    Add signup feature test

--- App/app.rb
+++ App/app.rb
@@ -1,6 +1,19 @@
 require 'sinatra/activerecord'
 require 'sinatra/base'
+require_relative './models/user'
+require_relative './models/spaces'
 
 class Makersbnb < Sinatra::Base
   enable :sessions
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    session[:user] = User.create(username: params["username"], email: params["email"], password: params["password"])
+    @user = session[:user]
+    erb :'/index' #connect to team
+  end
+
 end
