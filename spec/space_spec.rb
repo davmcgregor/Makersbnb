d43e29a030eb6e95b4d58d96adec266a5823574c
BarriF13 <info@bdesignstudio.co.uk>
2020-02-25 15:20:58 +0000

    Refactor html on spaces
    Ruby loop uses divs instead of lists for easier formatting.

--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -1,10 +1,10 @@
 
 describe Space do
-  it " we can create a space" do
+  it "creates a space" do
    space = Space.create(name: "name", description: "description", price: "price")
     expect(space).to be_a Space
   end
-  it'retreive list of spaces' do
+  it'retreives a list of spaces' do
     Space.create(name: "name", description: "description", price: "price")
     Space.create(name: "name", description: "description", price: "price")
 
