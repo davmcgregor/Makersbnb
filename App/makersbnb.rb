67f4ab3d1e62e7610be6727a85be84ff857d61f7
Ellie Richardson-Jones <elliebethjones@gmail.com>
2020-02-26 11:28:17 +0000

    Add sign in function

--- App/makersbnb.rb
+++ App/makersbnb.rb
@@ -5,12 +5,18 @@ require 'sinatra/base'
 require_relative './models/user'
 require_relative './models/spaces'
 require 'json'
+require 'sinatra/flash'
 
 ActiveRecord::Base.establish_connection(adapter: 'postgresql', database: 'makersbnb_development')
 
 class Makersbnb < Sinatra::Base
-  # register Sinatra::ActiveRecordExtension
+  register Sinatra::ActiveRecordExtension
+  register Sinatra::Flash
   enable :sessions
+
+  get '/' do
+    erb :index
+  end
 
   get '/spaces' do
     @user = (User.find_by id: session[:user_id])
@@ -27,6 +33,21 @@ class Makersbnb < Sinatra::Base
     user = User.create(username: params["username"], email: params["email"], password: encrypted_password)
     session[:user_id] = user.id
     redirect '/spaces'
+  end
+
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  post '/sessions/new' do
+    user = User.where({ username: params[:username] }).first
+    if user && BCrypt::Password.create(user.password) == params[:password]
+      session[:user_id] = user.id
+      redirect '/spaces'
+    else
+      flash[:notice] = 'Please check your email or password'
+      redirect '/sessions/new'
+    end
   end
 
   get '/spaces/new' do
